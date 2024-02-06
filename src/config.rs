f274f8f92697146bb6dcaaa7eec7e56c03533341
ROBERTS_PC <rob5300@hotmail.com>
2024-02-06 23:41:35 +0000

    Add regex crate and add some more json loading into structs.

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,18 @@
+use std::error::Error;
+
 use json::{object, JsonError, JsonValue, array};
+use regex::Regex;
 
 pub struct Config
 {
     json: JsonValue,
+}
+
+pub struct VpkEntry
+{
+    regex: Regex,
+    name: String,
+    args: String
 }
 
 impl Config 
@@ -12,8 +22,8 @@ impl Config
             json: object!{
                 vmt_path: "./bin/vpk.exe",
                 vpks: array![
-                    object!{regex: ".*", name: "all.vpk"},
-                    object!{regex: "^_+", name: "some.vpk"}
+                    object!{regex: ".*", name: "all.vpk", args: "-P"},
+                    object!{regex: "^_+", name: "some.vpk", args: "-P"}
                 ]
             }
         }
@@ -25,10 +35,35 @@ impl Config
     }
 }
 
+impl VpkEntry {
+    pub fn create(json_value: &JsonValue) -> VpkEntry {
+        VpkEntry {
+            args: json_value["args"].to_string(),
+            name: json_value["name"].to_string(),
+            regex: Regex::new(&json_value["regex"].to_string()).unwrap(),
+        }
+    }
+}
+
 impl Config
 {
     pub fn vmt_path(&self) -> String {
         self.json["vmt_path"].to_string()
+    }
+
+    pub fn get_vpk_entries(&self) -> Result<Vec<VpkEntry>, Box<dyn Error>>{
+        let vpks = &self.json["vpks"];
+        if vpks.is_array() {
+            let mut vpks_vec  = Vec::<VpkEntry>::new();
+            for entry in vpks.members() {
+                vpks_vec.push(VpkEntry::create(entry));
+            }
+
+            Ok(vpks_vec)
+        }
+        else {
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "vpks value is not an array?").into())
+        }
     }
 
     ///Convert config to json string
