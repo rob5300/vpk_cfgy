2662b6e8d0a585994e8bdd4c0c7da9cfcadb95be
ROBERTS_PC <rob5300@hotmail.com>
2024-02-11 20:44:33 +0000

    Use temp file for vpk file arg passing and improve config options and parsing. Add custom errors for some config errors.

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, io, rc::Rc};
 
 use json::{object, JsonError, JsonValue, array};
 use regex::Regex;
@@ -6,14 +6,24 @@ use regex::Regex;
 pub struct Config
 {
     json: JsonValue,
-    pub vmt_path: String
+    pub vpk_path: String,
+    pub dir: Rc<String>
 }
 
 pub struct VpkEntry
 {
     pub regex: Regex,
+    pub dir_regex: Regex,
     pub name: String,
     pub args: String
+}
+
+fn get_string(json: &JsonValue, key_name: &str) -> String {
+    let val = &json[key_name];
+    match val.is_null() {
+        true => "".to_owned(),
+        false => val.to_string()
+    }
 }
 
 impl Config 
@@ -21,30 +31,44 @@ impl Config
     pub fn create_default() -> Config {
         Config{
             json: object!{
-                vmt_path: "./bin/vpk.exe",
+                vpk_path: "./bin/vpk.exe",
+                dir: "",
                 vpks: array![
-                    object!{regex: ".*", name: "all.vpk", args: "-P"},
-                    object!{regex: "^_+", name: "some.vpk", args: "-P"}
+                    object!{regex: ".*", dir_regex: ".*", name: "all.vpk", args: "-P"},
+                    object!{regex: "(_low)$", dir_regex: ".*", name: "low.vpk", args: "-P"}
                 ]
             },
-            vmt_path: "".to_string()
+            vpk_path: "".to_string(),
+            dir: Rc::new("".to_owned()),
         }
     }
 
     pub fn create(json: &String) -> Result<Config, JsonError> {
         let parsed_obj = json::parse(json)?;
-        let vmt_path = parsed_obj["vmt_path"].to_string();
-        Ok(Config { json: parsed_obj, vmt_path: vmt_path })
+        let vmt_path = parsed_obj["vpk_path"].to_string();
+
+        let dir = get_string(&parsed_obj, "dir_regex");
+        Ok(Config { json: parsed_obj, vpk_path: vmt_path, dir: Rc::new(dir) })
     }
 }
 
 impl VpkEntry {
-    pub fn create(json_value: &JsonValue) -> VpkEntry {
-        VpkEntry {
+    pub fn create(json_value: &JsonValue) -> Result<VpkEntry, Box<dyn Error>> {
+        let regex_string = get_string(&json_value, "regex");
+        if regex_string.len() == 0 {
+            return Err(Box::new(io::Error::new(io::ErrorKind::InvalidData, "'regex' string was empty or missing")));
+        }
+        let dir_regex_string = get_string(&json_value, "dir_regex");
+        if dir_regex_string.len() == 0 {
+            return Err(Box::new(io::Error::new(io::ErrorKind::InvalidData, "'dir_regex' string was empty or missing")));
+        }
+
+        Ok(VpkEntry {
             args: json_value["args"].to_string(),
             name: json_value["name"].to_string(),
-            regex: Regex::new(&json_value["regex"].to_string()).unwrap(),
-        }
+            regex: Regex::new(&regex_string)?,
+            dir_regex: Regex::new(&dir_regex_string)?
+        })
     }
 }
 
@@ -54,8 +78,13 @@ impl Config
         let vpks = &self.json["vpks"];
         if vpks.is_array() {
             let mut vpks_vec  = Vec::<VpkEntry>::new();
+            let mut index = 0;
             for entry in vpks.members() {
-                vpks_vec.push(VpkEntry::create(entry));
+                vpks_vec.push(match VpkEntry::create(entry) {
+                    Ok(x) => {x},
+                    Err(e) => {return Err(Box::new(io::Error::new(io::ErrorKind::InvalidData, format!("Error in vpk entry #{index}: {e}"))));},
+                });
+                index += 1;
             }
 
             Ok(vpks_vec)
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,178 @@
 use config::Config;
-use std::ffi::OsStr;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::error::Error;
-use std::fs;
+use std::env;
 use std::process::Command;
+use std::process::ExitStatus;
+use walkdir::WalkDir;
+use tempfile::{self, NamedTempFile};
+use std::thread;
+use std::time;
 
 mod config;
 
 fn main() {
-    println!("[VPK cfgy]");
+    println!("[VPK cfgy] by rob5300");
 
-    let path = Path::new("./config.json").canonicalize().unwrap();
-    let display = path.display();
-    if path.exists() {
-        let mut file = match File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", display, why),
+    let args: Vec<String> = env::args().collect();
+    let exe_path = Path::new(&args[0]).parent().unwrap();
+    let mut working_path = match args.len() >= 2 {
+        true => Path::new(&args[1]),
+        false => Path::new(".")
+    };
+
+    let config_path = exe_path.join("config.json");
+    if config_path.exists() {
+        let mut file = match File::open(&config_path) {
+            Err(why) => panic!("Failed to open config at {}: {}", config_path.canonicalize().unwrap().display(), why),
             Ok(file) => file,
         };
         
         let mut s = String::new();
         file.read_to_string(&mut s).unwrap();
         
-        println!("-> Loaded config from '{}'", display);
+        println!("-> Loaded config from '{}'", config_path.canonicalize().unwrap().display());
         let config = Config::create(&s).unwrap();
 
-        let vmt_executable_path = Path::new(&config.vmt_path);
+        if config.dir.as_ref().len() > 0 {
+            working_path = Path::new(config.dir.as_ref());
+        }
+
+        let vmt_executable_path = Path::new(&config.vpk_path);
         if !vmt_executable_path.exists() {
             panic!("The value for 'vmt_path' in the config does not exist");
         }
 
         if cfg!(target_os = "windows") && vmt_executable_path.extension().unwrap() != "exe" {
-            
+            panic!("vmt path is not to an executable?");
         }
 
-        match process_vpk_entries(&config, ".".to_string()) {
+        match process_vpk_entries(&config, &working_path) {
             Ok(_) => {},
-            Err(e) => {println!("Failed to process vpk entries and execute vpk executable: {e}")},
+            Err(e) => {println!("{e}"); return;},
         }
     }
     else {
         let config = Config::create_default();
-        let mut new_config_file = match File::create(&path) {
+        let mut new_config_file = match File::create(&config_path) {
             Ok(file) => file,
             Err(_) => panic!("Failed to write config file"),
         };
         new_config_file.write_all(config.to_json().as_bytes()).unwrap();
-        println!("-> Wrote default config to '{}'", display);
+        println!("-> Wrote default config to '{}'", config_path.canonicalize().unwrap().display());
     }
 }
 
-fn process_vpk_entries(config: &Config, working_dir: String) -> Result<(), Box<dyn Error>>
+fn process_vpk_entries(config: &Config, working_dir: &Path) -> Result<(), Box<dyn Error>>
 {
-    let working_dir_path = Path::new(&working_dir);
-    println!("Working dir: '{}'", working_dir_path.display());
+    println!("-> Working dir will be '{}'", working_dir.display());
 
     let vpk_entries = config.get_vpk_entries()?;
+    let mut entry_num = 0;
     for entry in vpk_entries {
         //Filter files that match this vpk entry regex
-        let mut matching_files: Vec<String> = fs::read_dir(working_dir_path)?.filter_map(|f| {
-            let path = f.ok()?.path();
-            if path.is_file() && entry.regex.is_match(path.file_name()?.to_str()?) {
-                Some(path.to_str()?.to_string())
+        let regex_str = entry.regex.as_str();
+        println!("#{} Finding files for vpk entry with regex '{}' ...", entry_num, &regex_str);
+        let matching_files: Vec<String> = WalkDir::new(working_dir).into_iter().filter_map(|f| {
+            let dir_entry = f.unwrap();
+            let path = dir_entry.path();
+            let relative_path = path.strip_prefix(working_dir).unwrap();
+
+            if path.is_file() && entry.regex.is_match(path.file_name()?.to_str()?) && entry.dir_regex.is_match(relative_path.parent().unwrap().to_str()?) {
+                //Create quote wrapped string for path
+                let new_path_arg = relative_path.to_str()?.to_owned();
+                Some(new_path_arg)
             }
             else {
                 None
             }
         }).collect();
 
-        let mut args: Vec<String> = entry.args.split(" ").map(|s| s.to_string()).collect();
-        args.append(&mut matching_files);
-        Command::new(&config.vmt_path).args(args).output()?;
+        println!("Found {} files", matching_files.len());
+
+        //Skip when 0 files match
+        if matching_files.len() == 0 {
+            continue;
+        }
+
+        //Build arg list first with user arg strin
+        let vpk_name = entry.name;
+        if cfg!(debug_assertions) {
+            println!("[Debug] VPK Name: '{}'.", vpk_name)
+        }
+
+        let mut args = Vec::<String>::new();
+
+        if entry.args.len() > 0 {
+            let split_args = entry.args.split(" ");
+            for arg in split_args {
+                args.push(arg.to_string());
+            }
+        }
+
+        args.push("a".to_owned());
+        args.push(vpk_name.clone());
+
+        //Add response file arg
+        let response_file = get_files_response_file(&matching_files)?;
+        let response_file_tmp_path = response_file.into_temp_path();
+        let response_file_path_str = response_file_tmp_path.to_str().ok_or("Failed to get path for temp response file")?;
+
+        if cfg!(debug_assertions) {
+            println!("[Debug] Temp response file location is '{response_file_path_str}'.")
+        }
+
+        let response_file_arg = "@".to_owned() + response_file_path_str;
+        args.push(response_file_arg);
+
+        if cfg!(debug_assertions) {
+            println!("[Debug] Args: '{}'.", args.join(" "));
+        }
+
+        match execute_vpk(config, working_dir, args) {
+            Ok(exit_status) => {
+                if exit_status.success() {
+                    println!("VPK created with name '{}'", &vpk_name);
+                }
+            },
+            Err(e) => {println!("⚠️ Failed to execute vpk: {e}")},
+        };
+        entry_num += 1;
     }
 
     Ok(())
+}
+
+fn execute_vpk(config: &Config, working_dir: &Path, args: Vec<String>) -> Result<ExitStatus, Box<dyn Error>> {
+    println!("-- 🚀 Executing VPK --");
+    let mut command = Command::new(&config.vpk_path)
+    .current_dir(working_dir)
+    .args(args)
+    .spawn()?;
+
+    while command.try_wait()?.is_none() {
+        println!("...Waiting...");
+        thread::sleep(time::Duration::from_millis(500));
+    }
+    let exit_status = command.wait()?;
+    if exit_status.success() {
+        println!("-- ✅ VPK Completed Successfully --");
+    }
+    else {
+        println!("-- ❌ VPK Failed to complete --");
+    }
+    Ok(exit_status)
+}
+
+fn get_files_response_file(paths: &Vec<String>) -> Result<NamedTempFile, Box<dyn Error>>
+{
+    let mut file = NamedTempFile::new()?;
+    for path in paths {
+        let line = path.to_owned() + "\n";
+        file.write(&line.as_bytes())?;
+    }
+
+    Ok(file)
 }
